341e158c1df6a462870402f71356f30d7c2d926f
hasagiiiiii <100549155+hasagiiiiii@users.noreply.github.com>
2022-07-11 22:17:22 +0700

    dung an lz

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-xdsa
+
+// const img1= document.querySelector(".img1");
+// // const img1= document.getElementById("")
+// function zoomImg(e){
+//     // img1.style.width="200px"
+//     // img1.style.height="200px"
+//     img1.classList.add("zoomIn")
+// }
+// // onclick
+// img1.addEventListener("click",zoomImg);
+//onChange
+// const email = document.querySelector("#email");
+// const password = document.querySelector("#password");
+// email.addEventListener('change',handleChange)
+// function handleChange(e){
+//     console.log(e.target.value);
+// }
+// password.addEventListener('change',handleChange);
+//  -------------brad traversy media----------------
+
+
+ 
